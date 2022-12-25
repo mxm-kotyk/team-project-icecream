14d9bd25593f4bebf92cb2bc24ff379710ae3f62
Matvei212 <q841479@gmail.com>
2022-12-25 22:38:50 +0200

    добавил js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+ 
+
+(() => {
+  const refs = {
+    openModalBtns: document.querySelectorAll("[data-modal-about-product-open]"),
+    closeModalBtn: document.querySelector("[data-modal-about-product-close]"),
+    modal: document.querySelector("[data-modal-about-product]"),
+  };
+
+  refs.openModalBtns.forEach(btn=>btn.addEventListener("click", toggleModal));
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("modal-about-product__backdrop--is-hidden");
+  }
+})();
