756ff27fc4bf6734b6b892c084ad9bcf6fd61346
Maksym Kotyk <mxm.kotyk@gmail.com>
2022-12-25 10:26:17 +0100

    Fixed JS for modal Buy Now

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,11 +1,11 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-buy-now-open]"),
+    openModalBtns: document.querySelectorAll("[data-modal-buy-now-open]"),
     closeModalBtn: document.querySelector("[data-modal-buy-now-close]"),
     modal: document.querySelector("[data-modal-buy-now]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtns.forEach(btn=>btn.addEventListener("click", toggleModal));
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
